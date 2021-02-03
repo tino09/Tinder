e9d9f7e8d20a76769e0dcaf8a4bedf1cb26c7445
Valentine Ayim <valentineayimodu@gmail.com>
2021-02-03 20:55:20 +0100

    3/2/2021

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     <div className="app">
     
 
-      {/*Header*/}
+      
       <Header />
       <TinderCards />
-      {/*SwipeButtons*/}
+      <SwipeButtons />
       
     </div>
     
